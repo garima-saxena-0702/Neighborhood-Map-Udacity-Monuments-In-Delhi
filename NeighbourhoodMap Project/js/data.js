b539b6c99ce6d4f673e14d802826ef08853d58aa
Garima Saxena <garimasaxena0702@gmail.com>
2017-10-27 12:30:02 +0530

    Add files via upload

---
+++ NeighbourhoodMap Project/js/data.js
@@ -0,0 +1,58 @@
+//adding name and location of monuments
+var initialLocations = [{
+        "name": "National Zoological park Delhi",
+        "location": {
+            "lat": 28.608632,
+            "lng": 77.246680
+        }
+    },
+    {
+        "name": "Humayun Tomb",
+        "location": {
+            "lat": 28.598383,
+            "lng": 77.250285
+        }
+    },
+    {
+        "name": "Purana Quila",
+        "location": {
+            "lat": 28.614739,
+            "lng": 77.243247
+        }
+    },
+    {
+        "name": "National Gallery of Modern Art",
+        "location": {
+            "lat": 28.609917,
+            "lng": 77.234492
+        }
+    },
+    {
+        "name": "Akshardham (Delhi)",
+        "location": {
+            "lat": 28.612780,
+            "lng": 77.277064
+        }
+    },
+    {
+        "name": "India Gate",
+        "location": {
+            "lat": 28.613192,
+            "lng": 77.229471
+        }
+    },
+    {
+        "name": "Red Fort",
+        "location": {
+            "lat": 28.656159,
+            "lng": 77.241010
+        }
+    },
+    {
+        "name": "Agrasen Ki Baoli",
+        "location": {
+            "lat": 28.626113,
+            "lng": 77.224966
+        }
+    }
+];
